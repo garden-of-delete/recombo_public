25cf5d9db9cef328995d3a2737a6c529b65fb036
MBA <michelle.flanner@gmail.com>
2017-09-12 17:02:46 -0700

    MOD, TESTED:utilized clk's (super class of clkBfacfConformation3.clkBfacfConformation3Component) overloaded equivalence operator to test equivalence of recombinationCriteriaTest.preConformation and .postConformation

--- src/tests/recomboCriteriaTests.cpp
+++ src/tests/recomboCriteriaTests.cpp
@@ -67,10 +67,11 @@ bool testParallelRecombination()
     //result.translate(v);
     //clkConformationBfacf3 postConformation(reclass.postConformationList);
     char orientation = 'p';
-    sites=rctc.preConformation.countRecomboSites(19,22,orientation);   
+    sites=rctc.preConformation.countRecomboSites(18,22,orientation);   
     rctc.preConformation.performRecombination(sitechoice);
-    ASSERT_DOUBLES_EQUAL(rctc.preConformation.size(),1,0);
-    clkConformationAsList result(rctc.preConformation.getComponent(0));
+    ASSERT_EQUAL(rctc.preConformation.size(),1);
+    ASSERT(rctc.preConformation.getComponent(0)==rctc.postConformation.getComponent(0));
+    //clkConformationAsList result(rctc.preConformation.getComponent(0));
     
     return true;
 }
